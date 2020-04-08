23a8847a072447e71133d89bd21d209f91031afe
nick55660711 <63327482+nick55660711@users.noreply.github.com>
2020-04-08 15:10:46 +0800

    DAY1

--- CSharp/Assets/car.cs
+++ CSharp/Assets/car.cs
@@ -2,6 +2,9 @@
 
 //註解：用來說明、紀錄等…，只會在此腳本內看到
 //摘要 summary:描述此內容，會在其他腳本內看到-提高維護性與擴充性
+//新增的C#放在專案內為藍圖
+//放在場景物件上為實體物件
+
 /// <summary>
 /// 定義汽車物件
 /// </summary>
@@ -15,15 +18,35 @@ public class car : MonoBehaviour
     //剎車 - 是否開啟 - 布林值bool
 
     //定義欄位語法:
-    //資料類型 欄位名稱;
-   
-    //預設值
+    //修飾值 資料類型 欄位名稱;
+    //公開 public:允許其他類別存取、顯示在屬性面板(Inspector)上
+    //私人 private(預設):不允許其他類別存取、不顯示在屬性面板(Inspector)上
+    
+    //預設值：
     //int、float為0
     //string為""
     //bool為false
-    int cc = 2000;
-    float weight = 100.9f;
-    string brand = "BMW";
-    bool brake = false;
-
+    //欄位屬性 Attribute:Unity 提供用於輔助欄位的功能
+    // 語法:[屬性名稱(值)]
+    // 標題[Header("字串")]
+    //範圍[Range(最小值、最大值)]
+    /// <summary>
+    /// 汽車CC數
+    /// </summary>
+    [Header("汽車的CC數"),Range(500,5000)]
+    public int cc = 2000;
+    /// <summary>
+    /// 重量
+    /// </summary>
+    [Tooltip("這個欄位是重量")]
+    public float weight = 100.90f;
+    /// <summary>
+    /// 品牌
+    /// </summary>
+    [Header("品牌")]
+    [Tooltip("車子的品牌")]
+    public string brand = "BMW";
+    [Header("剎車"),Tooltip("目前剎車狀態")]
+    public bool brake = false;
+    
 }
--- CSharp/Assets/class1.cs
+++ CSharp/Assets/class1.cs
@@ -8,6 +8,12 @@ public class class1 : MonoBehaviour
     //C#
     //1.大小寫視為不同 2.括號成對出現 () [] {} '' "" <>
 
+     [Header("car1")]
+            public car car1;
+    [Header("car2")]
+       public car car2;
+        
+
     //事件:在特定時間會以指定次數執行的方法(名稱為藍色)
     //開始事件:播放遊戲時執行一次 - 初始設定(Start)
     private void Start()
@@ -16,5 +22,15 @@ public class class1 : MonoBehaviour
         //輸出訊息("文字訊息");
         // C# 以分號結尾;
         print("hello,world");
+
+
+        //取得 get
+        print("car1 CC數" + car1.cc); print("car2品牌" + car2.brand);
+        
+        //設定 set
+        car1.weight = 999.9f;
+        car1.brake = true;
+        car1.brand= "toyota";
+        car1.cc = 9999;
     }
 }
