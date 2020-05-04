a5d92175880d441aa6016cdfa0d8204634c38eb7
nick55660711 <63327482+nick55660711@users.noreply.github.com>
2020-05-04 12:33:42 +0800

    practice2

--- CSharp/Assets/Practice1_OperatorMethod.cs
+++ CSharp/Assets/Practice1_OperatorMethod.cs
@@ -2,19 +2,19 @@
 using UnityEngine.UI; // 引用 Unity 介面 命名空間
 using System; // 引用 系統 命名空間(系統API字串轉整數)
 public class Practice1_OperatorMethod : MonoBehaviour
-{   
+{
     //儲存兩筆數字
     public string number1;
     public string number2;
-    
+
     [Header("文字：輸出結果")]
     public Text result;
-    
+
     /// <summary>
     /// 給第一個輸入欄位使用
     /// </summary>
     /// <param name="number">第一個欄位的數值</param> 
-    
+
     public void InputNumber1(string number)
     {
         number1 = number;
@@ -45,11 +45,11 @@ public class Practice1_OperatorMethod : MonoBehaviour
         // 輸出結果.文字內容 =
 
         result.text = ("加法結果：" + (n1 + n2));
-       
 
 
 
-    }    
+
+    }
     public void Minus()
     {
         // Int32.Prase(字串) － 將字串轉為整數
@@ -62,7 +62,7 @@ public class Practice1_OperatorMethod : MonoBehaviour
 
         result.text = ("結果：" + (n1 - n2));
 
-    } 
+    }
     public void Multiply()
     {
         // Int32.Prase(字串) － 將字串轉為整數
@@ -104,6 +104,6 @@ public class Practice1_OperatorMethod : MonoBehaviour
     }
 
 
-    
+
 
 }
