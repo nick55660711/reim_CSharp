aad4e617abdce2fc46668120e57b60adb0a9e791
nick55660711 <63327482+nick55660711@users.noreply.github.com>
2020-05-05 11:29:36 +0800

    practice2

---
+++ Practice2/Assets/bat.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class bat : MonoBehaviour
+{
+    [Header("bat血量"), Range(0, 200)]
+    public int batHP=100;
+    [Header("bat攻擊力")]
+    public int batATK=30;
+    [Header("bat回復量")]
+    public int batCure=15;
+
+    public Text Result;
+
+    public slime slime1;
+    public void batHit()
+    {
+        slime1.slimeDamage(batATK);
+    }
+    public void batDamage(int slimeATK)
+    {
+        batHP -= slimeATK;
+
+        Result.text = gameObject.name + "－受到傷害" + slimeATK + "\n" + gameObject.name + "－當前血量" + batHP;
+        
+    }
+    public void batRecovery()
+    {
+        batHP += batCure;
+
+        Result.text = gameObject.name + "－回復" + batCure + "\n" + gameObject.name + "－當前血量" + batHP;
+
+
+    }
+
+
+
+
+
+}
---
+++ Practice2/Assets/slime.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class slime : MonoBehaviour
+{
+    [Header("slime血量"), Range(0, 150)]
+    public int slimeHP = 40;
+    [Header("slime攻擊力")]
+    public int slimeATK = 10;
+    [Header("slime回復量")]
+    public int slimeCure = 25;
+
+    public Text Result;
+
+    public bat bat1;
+    public void slimeHit()
+    {
+        bat1.batDamage(slimeATK);
+
+
+    }
+    public void slimeDamage(int batATK)
+    {
+       
+        slimeHP -= batATK;
+
+        Result.text = gameObject.name + "－受到傷害" + bat1.batATK + "\n" + gameObject.name + "－當前血量" + slimeHP;
+
+        
+    }
+    public void slimeRecovery()
+    {
+        slimeHP += slimeCure;
+        
+        Result.text = gameObject.name + "－回復" + slimeCure + "\n" + gameObject.name + "－當前血量" + slimeHP;
+
+
+    }
+}
